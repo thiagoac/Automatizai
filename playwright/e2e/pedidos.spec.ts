906321fa91d5130bc0e53b2e261c7fa2bab58dd6
Thiago Augusto da Costa <thiago.btw@gmail.com>
2026-04-08 21:47:50 -0300

    criado um teste de consulta de pedidos

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test' 
+
+test('Deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/') 
+
+  // Checkpoint 1: Verificar se a página de consulta de pedidos está carregada
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint') 
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click() 
+
+  // Checkpoint 2: Verificar se a página de consulta de pedidos está carregada
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido') 
+
+  await page.getByTestId('search-order-id').fill('VLO-1YHZBP') 
+
+  await page.getByTestId('search-order-button').click() 
+
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-1YHZBP') 
+
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO') 
+})  
