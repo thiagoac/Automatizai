1ef617209041f55e58b7a86eebbc93522625db26
Thiago Augusto da Costa <thiago.acosta@gti654173.local>
2026-05-04 13:48:16 -0300

    criacao de teste para pedido em analise

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -17,7 +17,7 @@ test.describe('Consultar Pedido', () => {
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido') 
 
   })
-  
+
   
   test('Deve consultar um pedido aprovado', async ({ page }) => {
 
@@ -67,7 +67,103 @@ test.describe('Consultar Pedido', () => {
 
   }) 
 
-  test('Deve consultar um pedido rejeitado', async ({ page }) => {
+  test('Deve consultar um pedido reprovado', async ({ page }) => {
+
+    //Test Data
+    const orderId = 'VLO-IK5T3V';
+    
+    await page.getByTestId('search-order-id').fill(orderId) 
+
+    await page.getByTestId('search-order-button').click() 
+
+    // await expect(page.getByTestId('order-result-id')).toContainText(orderId) 
+
+    // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO') 
+
+    await expect(page.getByTestId(`order-result-${orderId}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${orderId}
+      - img
+      - text: REPROVADO
+    `);
+
+    await expect(page.getByTestId(`order-result-${orderId}`)).toMatchAriaSnapshot(`
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: Midnight Black
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: sport Wheels
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: Chris Gardner Costa
+      - paragraph: Email
+      - paragraph: chris@teste.com
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: À Vista
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+    `);
+
+
+  }) 
+
+  test('Deve consultar um pedido em análise', async ({ page }) => {
+
+    //Test Data
+    const orderId = 'VLO-TSMY11';
+    
+    await page.getByTestId('search-order-id').fill(orderId) 
+
+    await page.getByTestId('search-order-button').click() 
+
+    // await expect(page.getByTestId('order-result-id')).toContainText(orderId) 
+
+    // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO') 
+
+    await expect(page.getByTestId(`order-result-${orderId}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${orderId}
+      - img
+      - text: EM_ANALISE
+    `);
+
+    await expect(page.getByTestId(`order-result-${orderId}`)).toMatchAriaSnapshot(`
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: Midnight Black
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: sport Wheels
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: 2pac shakur
+      - paragraph: Email
+      - paragraph: 2pac@test.com
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: À Vista
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+    `);
+
+
+  }) 
+
+  test('Deve consultar um pedido inexistente', async ({ page }) => {
 
     //Test Data
     const orderId = gerarCodigoVLO()
@@ -82,6 +178,6 @@ test.describe('Consultar Pedido', () => {
       - paragraph: Verifique o número do pedido e tente novamente
       `);
 
-    }) 
+  }) 
 
 })
